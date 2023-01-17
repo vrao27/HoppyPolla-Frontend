19b762bcae2eb03504e66857d8b9c5a4bae94f8d
Vinay Rao <vrao8912@gmail.com>
2023-01-17 23:07:24 +0100

    comm

--- src/pages/Brew.js
+++ src/pages/Brew.js
@@ -1,4 +1,3 @@
-// import BrewRecipeDetails from "../components/BrewRecipeDetails";
 import BrewRecipesMap from "../components/BrewRecipesMap";
 import { Link } from "react-router-dom";
 import { useRecipe } from "../context/BrewRecipeContext";
@@ -9,26 +8,32 @@ import { useEffect } from "react";
 const BrewRecipe = () => {
   const { brewRecipes, setBrewRecipes } = useRecipe();
   const { isAuth } = useAuthContext();
+
   //console.log(brewRecipes);
 
+  //adding this extra layer so that if there is no user then the upon logout the login form is automatically re-rendered and the recipes are gon
 
-  //adding this extra layer so that if there is no user then the upon logout the login form is automatically re-rendered and the recipes are gon 
   useEffect(() => {
     if (!isAuth) {
       setBrewRecipes([]);
     }
   }, [isAuth]);
 
+  //Wrap the whole div so that it is rendered and the css is active when user logs in other set back to original state
   return (
-    <div className="brewRecipes container">
-      {brewRecipes &&
-        brewRecipes.map((brewRecipe) => (
-          <Link to={`/brew/${brewRecipe._id}`} key={brewRecipe._id}>
-            <BrewRecipesMap brewRecipe={brewRecipe} />
-          </Link>
-        ))}
+    <>
+      {isAuth && (
+        <div className="brewRecipes container">
+          {brewRecipes &&
+            brewRecipes.map((brewRecipe) => (
+              <Link to={`/brew/${brewRecipe._id}`} key={brewRecipe._id}>
+                <BrewRecipesMap brewRecipe={brewRecipe} />
+              </Link>
+            ))}
+        </div>
+      )}
       {!isAuth && <LoginMessage />}
-    </div>
+    </>
   );
 };
 
